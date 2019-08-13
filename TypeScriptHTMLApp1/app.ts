326d03e8d0e79230ca2b7638b046b2347c88417f
Shinichi Ueno <uen.agile@gmail.com>
2019-08-14 08:39:17 +0900

    vanilla ts Sample

---
+++ TypeScriptHTMLApp1/app.ts
@@ -0,0 +1,39 @@
+﻿// index.html の <head> タグが解釈されたタイミングで走破され、window.onload に処理が紐付けられる
+window.onload = () => {
+    let view = new MVVMSampleApp.IndexView();
+    view.Bind();
+};
+
+namespace MVVMSampleApp{
+    // View （ViewとViewModelを糊付けするクラス）
+    export class IndexView {
+        public ViewModel: IndexViewModel;
+        public Bind() {
+            this.ViewModel = new IndexViewModel();
+
+            let h = document.getElementById("message1");
+
+
+            let input = document.getElementById("input1") as HTMLInputElement;
+            input.onchange = e => {
+                this.ViewModel.InputValue = input.value;
+            };
+
+
+            let button1 = document.getElementById("button1");
+            button1.onclick = e => {
+                h.innerText = this.ViewModel.HelloAction();
+            };
+        }
+
+    }
+
+    // ViewModel （Viewを直接触らないスクリプト）
+    export class IndexViewModel {
+        public InputValue: string = "Hello MVVM!";
+        public HelloAction() {
+            return this.InputValue;
+        }
+    }
+}
+
